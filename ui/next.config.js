c5189f0504b41c53cad9e7409f66603e04aaaf71
Frode Hansen <frode.hansen@northern.tech>
2026-07-10 10:07:01 +0200

    chore: increase staticPageGenerationTimeout to avoid build failures
    Ticket: None
    
    Signed-off-by: Frode Hansen <frode.hansen@northern.tech>

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   output: 'export',
   reactStrictMode: true,
-  transpilePackages: ['@mui/x-data-grid']
+  transpilePackages: ['@mui/x-data-grid'],
+  staticPageGenerationTimeout: 180 // 3 minutes, 60 seconds have proved to cause timeouts frequently
 };
